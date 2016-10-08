e4aa9d765cb99c94d7df41ce42c576350bab0fe8
Creator <zkh90644@gmail.com>
2016-10-08 21:00:46 +0800

    rebuildAllCode

---
+++ zafu_iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  zafu_iOS
+//
+//  Created by zkhCreator on 08/10/2016.
+//  Copyright © 2016 zkhCreator. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
